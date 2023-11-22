eb53bfbc9cc3f19b701db1166a7da12db3570413
Semyon19988 <108398312+Semyon19988@users.noreply.github.com>
2023-11-22 18:03:37 +0300

    Created Tower Generation

---
+++ Assets/Scripts/TowerGeneration/Tower.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace TowerGeneration
+{
+	public class Tower
+	{
+		private readonly Queue<TowerSegment> _segments;
+		
+		public Tower(IEnumerable<TowerSegment> segments) : this(new Queue<TowerSegment>(segments)){ }
+		private Tower(Queue<TowerSegment> segments) =>
+			_segments = segments;
+	}
+}
