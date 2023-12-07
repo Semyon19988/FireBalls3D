4dc77042c04e47f12935a485557af6c9f5e17dfe
Semyon19988 <108398312+Semyon19988@users.noreply.github.com>
2023-12-07 17:26:00 +0300

    Created GameStateMachine

---
+++ Assets/Scripts/GameStates/States/LevelEntrySo.cs
@@ -0,0 +1,19 @@
+﻿using GameStates.Base;
+using UnityEngine;
+
+namespace GameStates.States
+{
+	public class LevelEntrySo : GameStateSo
+	{
+		[SerializeField] private string _levelName;
+		public override void Enter()
+		{
+			
+		}
+
+		public override void Exit()
+		{
+			
+		}
+	}
+}
