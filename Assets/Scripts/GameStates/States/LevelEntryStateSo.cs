d98551b57e22eebe8c71e907dd17d89c0acc7655
Semyon19988 <108398312+Semyon19988@users.noreply.github.com>
2023-12-07 19:55:25 +0300

    Created Scene Loading

---
+++ Assets/Scripts/GameStates/States/LevelEntryStateSo.cs
@@ -0,0 +1,24 @@
+﻿using GameStates.Base;
+using SceneLoading;
+using UnityEngine;
+
+namespace GameStates.States
+{
+	public class LevelEntryStateSo : GameStateSo
+	{
+		[SerializeField] private Scene _scene;
+
+		private readonly IAsyncSceneLoading _sceneLoading = new UnitySceneLoading();
+		
+		public override void Enter()
+		{ 
+			_sceneLoading.LoadAsync(_scene);
+			
+		}
+
+		public override void Exit()
+		{
+			
+		}
+	}
+}
---
+++ Assets/Scripts/SceneLoading/IAsyncSceneLoading.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SceneLoading
+{
+	public interface IAsyncSceneLoading
+	{
+		Task<AsyncOperation> LoadAsync(Scene scene);
+		Task<AsyncOperation> UnloadAsync(Scene scene);
+	}
+}
