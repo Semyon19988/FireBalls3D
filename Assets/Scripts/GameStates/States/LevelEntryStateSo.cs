48ccf964bb63612cd97f3a98e677631e1a2481d0
Semyon19988 <108398312+Semyon19988@users.noreply.github.com>
2023-12-10 15:12:06 +0300

    Created Game States Scriptable Ojects

--- Assets/Scripts/GameStates/States/LevelEntryStateSo.cs
+++ Assets/Scripts/GameStates/States/LevelEntryStateSo.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 namespace GameStates.States
 {
+	[CreateAssetMenu(fileName = "LevelEntryStateSo", menuName = "ScriptableObjects/Game/States/LevelEntryState")]
 	public class LevelEntryStateSo : GameStateSo
 	{
 		[SerializeField] private Scene _scene;
 
 		private readonly IAsyncSceneLoading _sceneLoading = new UnitySceneLoading();
 		
-		public override void Enter()
-		{ 
+		public override void Enter() => 
 			_sceneLoading.LoadAsync(_scene);
-			
-		}
 
-		public override void Exit()
-		{
-			
-		}
+		public override void Exit() { }
 	}
 }
---
+++ Assets/Scripts/Menu/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using GameStates.Base;
+using GameStates.States;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+	[RequireComponent(typeof(Button))]
+	public class PlayButton : MonoBehaviour
+	{
+		[SerializeField] private GameStateMachineSo _stateMachine;
+
+		private void Start()
+		{
+			Button button = GetComponent<Button>();
+			button.onClick.AddListener(_stateMachine.Enter<LevelEntryStateSo>);
+		}
+	}
+}
